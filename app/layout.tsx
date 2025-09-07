6f40bbc3b4187639c98004e3ebf07ce60f2dceaf
Stephen Matheis <stephen.a.matheis@gmail.com>
2025-09-07 14:38:08 -0400

    feat: inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Nunito, Nunito_Sans } from "next/font/google";
+import "./layout.scss";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
+const sans = Nunito_Sans({
+    variable: "--font-sans",
     subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
+const display = Nunito({
+    variable: "--font-display",
     subsets: ["latin"],
 });
 
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+            <body className={`${sans.variable} ${display.variable}`}>{children}</body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,63 @@
-import styles from "./page.module.css";
+'use client';
+
+import { useState } from 'react';
+import * as motion from 'motion/react-client';
+import styles from './page.module.scss';
 
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={styles.page}>
-            <h1>Tablet</h1>
+            {/* Background */}
+            <div className={styles.background} />
+
+            {/* Home Screen */}
+            <motion.div
+                className={styles.homescreen}
+                variants={{
+                    open: {
+                        y: '-100%',
+                    },
+                    closed: {
+                        y: 0,
+                    },
+                }}
+                initial="closed"
+                animate={isOpen ? 'open' : 'closed'}
+                transition={{ ease: 'linear', duration: 0.25 }}
+            >
+                {/* Status bar */}
+                <div className={styles.statusbar}>
+                    <div className={styles.network}>Verizon</div>
+                    <div className={styles.icons}>
+                        <div>Arrow</div>
+                        <div>Signal</div>
+                        <div>Wi-Fi</div>
+                        <div>Battery %</div>
+                        <div>Battery Visual</div>
+                    </div>
+                </div>
+                <div className={styles.view}>
+                    <div className={styles.widgets}>Widgets</div>
+                    <div className={styles.main}>
+                        <div className={styles.date}>Sun Sep 7</div>
+                        <div className={styles.time}>11:59</div>
+                    </div>
+                    <div className={styles.bar} onClick={() => setIsOpen(true)} />
+                </div>
+            </motion.div>
+
+            {/* Overlay */}
+            {/* NOTE: OFF */}
+            {/* <motion.div
+                className={styles.overlay}
+                initial={{
+                    opacity: 1,
+                }}
+                animate={{ opacity: 0 }}
+                transition={{ ease: "easeInOut", duration: 1, delay: 2 }}
+            /> */}
         </div>
     );
 }
